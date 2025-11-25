6c082af391fd8ecb9cba0e3054193d3ab021186d
Allyssa023 <allyssaashley.dulay@lorma.edu>
2025-11-25 17:05:01 +0800

    pakicopy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Sidebar from "./components/Sidebar1"
 import Header from "./components/Header1"
 import { useState } from "react"
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
 
 const App = () =>{
   const [sidebarToggle, setSidebarToggle] = useState(true);
@@ -12,10 +15,13 @@ const App = () =>{
   return (
     <div className="flex-1 flex h-screen bg-gray-100">
     <Sidebar isOpen = {sidebarToggle} />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col ">
         <Header onSidebarToggle = {toggleSidebar}/>
         <main className="flex-1 bg-slate-200">
-          Contents here
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+            <Route path="/products" element={<Products/>}/>
+          </Routes>
         </main>
       </div>
     </div>
--- src/components/Sidebar1.jsx
+++ src/components/Sidebar1.jsx
@@ -1,21 +1,33 @@
+import {Link, useLocation} from "react-router-dom"
 function Sidebar({isOpen}){
+    const location = useLocation();
     const menuItems = [
         {icon: "🏠", text: "Home", link: "/"},
-        {icon: "🛍", text: "Products", link: "/product"},
-        {icon: "⚙", text: "Settings", link: "/settings"}
+        {icon: "👜", text: "Products", link: "/product"},
+        // {icon: "⚙", text: "Settings", link: "/settings"}
     ]
 
     return  (
-    <div className={`${isOpen ? 'w-36 p-4': 'w-0 p-0'} overflow-hidden bg-gray-900 text-white transition-all duration-300`}>
+    <div className={`${isOpen ? 'w-64': 'w-0'} overflow-hidden bg-gray-900 text-white transition-all duration-300`}>
         <div className="p-4">
-            <h2>KaaBUY</h2>
+            <h2 className="text-2xl font-bold">KaaBUY</h2>
         </div>
-        <nav className="p-4">
-            <ul>
+        <nav className="mt-6">
+            
             {
-                menuItems.map((item, index) => (<li key={index}><i>{item.icon}</i>{item.text}</li>)) 
+                menuItems.map((item, index) => (
+                    <Link
+                        key={index}
+                        to={item.link}
+                        className={`flex items-center px-6 py-3 transtition-colors ${location.pathname == item.link ? 'bg-gray-800 border-l-4 border-blue-500' : 'hover:bg-gray-800'}`}
+                    >
+                        <span className="text-xl mr-3">{item.icon}</span>
+                        {item.text}
+                    </Link>
+
+                )) 
             }   
-            </ul>
+            
         </nav>
     </div>
     )
