4d1a96f24df3b745affdabd7c3df17245b271c69
manofsteeljj <justinjoseph.sanchez@lorma.edu>
2025-11-25 17:32:11 +0800

    sidebar completed final

--- kaabuy-app/src/App.jsx
+++ kaabuy-app/src/App.jsx
@@ -1,18 +1,33 @@
 import Sidebar from "./components/Sidebar"
 import Header from "./components/Header"
-import { useState} from "react"
+import { useState } from "react"
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Product";
+import Settings from "./pages/Settings";
 
 const App = () =>{
-  const [sidebarToggle, setSidebarToggle] = useState(true)
+  const [sidebarToggle, setSidebarToggle] = useState(true);
 
   function toggleSidebar(){
     setSidebarToggle(!sidebarToggle)
   }
-  return(<>
-    <Sidebar status = {sidebarToggle}/>
-    <Header onSidebarToggle = {toggleSidebar} />
-    </>
+
+  return (
+    <div className="flex-1 flex h-screen bg-gray-100">
+    <Sidebar isOpen = {sidebarToggle} />
+      <div className="flex-1 flex flex-col ">
+        <Header onSidebarToggle = {toggleSidebar}/>
+        <main className="flex-1 bg-slate-200">
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+            <Route path="/products" element={<Products/>}/>
+            <Route path="/settings" element={<Settings/>}/>
+          </Routes>
+        </main>
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App 
--- kaabuy-app/src/components/Sidebar.jsx
+++ kaabuy-app/src/components/Sidebar.jsx
@@ -1,25 +1,41 @@
-function Sidebar({status}){
+import {Link, useLocation} from "react-router-dom"
+function Sidebar({isOpen}){
+    const location = useLocation();
     const menuItems = [
         {icon: "🏠", text: "Home", link: "/"},
-        {icon: "🛍", text: "Products", link: "/products"},
-        {icon: "⚙", text: "Settings", link: "/settings"},
+        {icon: "👜", text: "Products", link: "/products"},
+        {icon: "⚙", text: "Settings", link: "/settings"}
     ]
 
-    return status ? (
-    <div className="bg-gray-900">
-        <div>
-            <h2>KaaBUY</h2>
+    return  (
+    <div className={`${isOpen ? 'w-64': 'w-0'} overflow-hidden bg-gray-900 text-white transition-all duration-300`}>
+        <div className="p-4">
+            <h2 className="text-2xl font-bold">KaaBUY</h2>
         </div>
-        
-        <nav>
-            <ul>
+        <nav className="mt-6">
+            
             {
-                menuItems.map((item, index) =>(<li key={index}><i>{item.icon}</i>{item.text}</li>))
-            }
-            </ul>
+                menuItems.map((item, index) => (
+                    <Link
+                        key={index}
+                        to={item.link}
+                        className={`flex items-center px-6 py-3 transition-colors border-l-4
+                        ${location.pathname === item.link 
+                            ? 'bg-orange-800 border-orange-500' 
+                            : 'border-transparent hover:bg-orange-800'}
+                        `}
+
+                    >
+                        <span className="text-xl mr-3">{item.icon}</span>
+                        {item.text}
+                    </Link>
+
+                )) 
+            }   
+            
         </nav>
     </div>
-    ) : null
+    )
 }
 
 export default Sidebar;
